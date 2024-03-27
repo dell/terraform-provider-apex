83ae52068fc2179e53e360d02930ed08462aa4b3
doriac11 <Connor.Doria@Dell.com>
2024-03-27 10:46:23 -0400

    Remove token-files, update example files
    - Remove the token_file.txt in favor of just reading it from the
      provider token input paramteter.
    - Update example files, split into provider, variable, then
      resource/datasource files. Update the example file directory names
      to have the `navigator_block` prefix.

---
+++ examples/main.tf
@@ -0,0 +1,45 @@
+/*
+Copyright (c) 2024 Dell Inc., or its subsidiaries. All Rights Reserved.
+
+Licensed under the Mozilla Public License Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://mozilla.org/MPL/2.0/
+
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+terraform {
+  required_providers {
+    apex = {
+      source  = "dell/apex"
+    }
+  }
+}
+
+provider "apex" {
+  host  = var.HOST
+  token = var.JWT_TOKEN
+  jms_endpoint = var.JMS_ENDPOINT
+}
+
+variable "JWT_TOKEN" {
+  type = string
+  default = "insert-token-here"
+}
+
+variable "HOST" {
+  type = string
+  default = "https://example-host.com"
+}
+
+variable "JMS_ENDPOINT" {
+  type = string
+  default = "https://example-jobs-endpoint.com"
+}
